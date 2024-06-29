45cd38c9599e64fed22ca87c7e311347815b4fcc
David Martinez Bahillo <dmb1004@alu.ubu.es>
2024-06-29 17:57:26 +0200

    eliminación aplición backUp

--- Code/AppContractBackUp/src/ether/web3.js
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-import ABI from './MyContractAux.json'
-
-const provider = new ethers.providers.JsonRpcProvider("https://sepolia.infura.io/v3/d2fa416c31e348ef82c706d0a1e24fc8");
-const contractAddress = "0x3eA2717cf5AE3ccc89d868fB317aE938b6aC8EBc";
-const Mycontract = new ethers.Contract(contractAddress, ABI, provider);
-
-export { Mycontract, ABI, contractAddress };
